9c99bc53f43084a3afd7ff908cbf952b50ea9c7f
cjedsell1122 <ce897223@ohio.edu>
2024-09-05 15:49:02 -0400

    Added program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Christine Edsell
+ *        Date: 9/5/2024
+ * Description: git-lab program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
